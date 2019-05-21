ff874d8b09188dc889b99177f9de4310e0af580e
Zubarev Ivan <zubarei1@nbe-045.org.aalto.fi>
2019-05-21 11:58:56 +0300

    removed tracked *.pyc files, minor fixes to models and utils

--- mneflow/__init__.py
+++ mneflow/__init__.py
@@ -1,4 +1,4 @@
 from . import models
 from . import layers
-from .utils import produce_tfrecords, load_meta#, leave_one_subj_out, logger, plot_cm
+from .utils import produce_tfrecords, load_meta, leave_one_subj_out#, logger, plot_cm
 
