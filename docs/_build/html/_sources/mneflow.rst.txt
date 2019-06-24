e788292a9c5e53df9cb2c32abfe0da08a851f58c
Zubarev Ivan <zubarei1@nbe-045.org.aalto.fi>
2019-06-24 15:31:45 +0300

    added docs, udpated to 0.1.1-beta

---
+++ docs/_build/html/_sources/mneflow.rst.txt
@@ -0,0 +1,40 @@
+API Reference
+=============
+
+mneflow.models
+--------------
+
+.. automodule:: mneflow.models
+.. autoclass:: Model
+    :members:
+.. autoclass:: LFCNN
+    :members:
+.. autoclass:: VARCNN
+    :members:
+.. autoclass:: EEGNet
+    :members:
+
+
+mneflow.Dataset
+---------------
+.. automodule:: mneflow.data
+.. autoclass:: Dataset
+    :members:
+
+
+mneflow.Optimizer
+-----------------
+
+.. automodule:: mneflow.optimize
+.. autoclass:: Optimizer
+    :members:
+
+mneflow.utils 
+-------------
+
+.. automodule:: mneflow.utils
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
--- mneflow/optimize.py
+++ mneflow/optimize.py
@@ -9,20 +9,40 @@ import tensorflow as tf
 
 
 class Optimizer(object):
-    def __init__(self, params):
-        self.params = params
+    """
+    Optimizer object.
+
+    """
+    def __init__(self, learn_rate=3e-4, l1_lambda=0, l2_lambda=0,
+                 task='classification'):
+        """
+        Parameters
+        ----------
+        learn_rate : float
+                    learning rate
+        l1_lambda : float, optional
+                    coefficient for sparse penaly on hte parameter weights
+
+        task : str, {'classification', 'regression'}
+
+
+        """
+        self.params = dict(learn_rate=learn_rate, l1_lambda=l1_lambda,
+                           l2_lambda=l2_lambda, task=task)
         # TODO : add cost function options,
         # TODO : class balance
         # TODO : regularization options,
         # TODO : performance metric options
 
-    def _set_optimizer(self, y_pred, y_true):
-        """Initializes the optimizer part of the computational graph
+    def set_optimizer(self, y_pred, y_true):
+
+        """
+        Initializes the optimizer part of the computational graph
 
         This method can be overriden a for custom optimizer
 
-        Inputs:
-        -------
+        Parameters
+        ----------
         y_pred : tf.Tensor
                         predictions of the target varible, output of the
                         computational graph
@@ -30,7 +50,7 @@ class Optimizer(object):
         y_true : tf.Tensor
                         target_variable, output of dataset.iterator
 
-        Returns:
+        Returns
         --------
         train_step : tf.Operation
                     training operation
@@ -67,20 +87,15 @@ class Optimizer(object):
             reg = [tf.reduce_sum(tf.abs(var))
                    for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
                    if 'weights' in var.name]
+            cost = cost + coef * tf.add_n(reg)
         elif self.params['l2_lambda'] > 0:
             coef = self.params['l2_lambda']
             reg = [tf.nn.l2_loss(var) for var in
                    tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
                    if 'weights' in var.name]
-        else:
-            coef = 0
-            reg = 0
-
-        weight_penalty = coef * tf.add_n(reg)
-        cost = cost + weight_penalty
+            cost = cost + coef * tf.add_n(reg)
 
         #  Optimizer
-        backprop = tf.train.AdamOptimizer
-        train_step = backprop(self.params['learn_rate']).minimize(cost)
+        train_step = tf.train.AdamOptimizer(learning_rate=self.params['learn_rate']).minimize(cost)
 
         return train_step, performance, cost, prediction
