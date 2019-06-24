db6ac184f3d803e7470766dea31ad7439c63f38b
Zubarev Ivan <zubarei1@nbe-045.org.aalto.fi>
2019-06-24 19:38:41 +0300

    even more docs

--- docs/conf.py
+++ docs/conf.py
@@ -29,8 +29,18 @@ def skip(app, what, name, obj, would_skip, options):
         return False
     return would_skip
 
+def run_apidoc(_):
+	from sphinx.apidoc import main
+	import os
+	import sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+	cur_dir = os.path.abspath(os.path.dirname(__file__))
+	module = os.path.join(cur_dir,"..","labbookdb")
+	main(['-e', '-o', cur_dir, module, '--force'])
+
 def setup(app):
     app.connect("autodoc-skip-member", skip)
+    app.connect('builder-inited', run_apidoc)
 
 # -- General configuration ---------------------------------------------------
 
