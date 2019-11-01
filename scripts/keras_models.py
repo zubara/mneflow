b0b88d942b8dd55fd9012201d99500c2d6fe597e
Gavriela Vranou <gavriela.vranou@aalto.fi>
2019-11-01 13:30:36 +0200

    Changes on keras adapted models
    @keras_layers:
    + Added l1 and l2 regularization on layers
    + Added axis argument on Demix layer to mirror mneflow version
    + Added DeConvLayer
    
    @keras_models:
    + Added l1 and l2 regularization
    + Added crude implementation of VARDAE
    - Removed Dense output layer from VARCNN-LSTM model
    * LSTM adjusts the input to always be a 4D tensor
    * Moved utility functions to keras_utils.py

--- scripts/keras_models.py
+++ scripts/keras_models.py
@@ -9,12 +9,10 @@ import tensorflow as tf
 # tf.enable_eager_execution()
 
 import numpy as np
-from keras_layers import DeMixing, VARConv, LSTMv1, Dense
+from keras_layers import DeMixing, VARConv, LSTMv1, Dense, DeConvLayer
 
 
 class VARCNNLSTM(tf.keras.Model):
-    # TODO: not tested yet
-
     """VAR-CNN-LSTM
 
     For details see [1].
@@ -52,45 +50,91 @@ class VARCNNLSTM(tf.keras.Model):
         [1]  I. Zubarev, et al., Adaptive neural network classifier for
         decoding MEG signals. Neuroimage. (2019) May 4;197:425-434
         """
-    def __init__(self, specs, name='var-cnn', **kwargs):
+    def __init__(self, specs, name='varcnn-lstm', **kwargs):
         """
         Parameters
         -----------
         specs : dict
-                dictionary of model-specific hyperparameters. Must include at
-                least model_path - path for saving a trained model. See
-                subclass definitions for details.
+                dictionary of model-specific hyperparameters.
 
         """
-        super(VARCNN, self).__init__(name=name, **kwargs)
+        super(VARCNNLSTM, self).__init__(name=name, **kwargs)
         self.scope = name
         self.specs = specs
+        self._layers.remove(self.specs)  # Weirdly it's considered as a layer
         self.model_path = specs['model_path']
         self.rate = specs['dropout']
         self.out_dim = specs['out_dim']
+        self.l1_l2 = tf.keras.regularizers.l1_l2(l1=self.specs['l1'],
+                                                 l2=self.specs['l1'])
 
-        self.demix = DeMixing(n_ls=self.specs['n_ls'])
+        self.demix = DeMixing(n_ls=self.specs['n_ls'],
+                              axis=specs['axis'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
+
         self.tconv1 = VARConv(scope="conv", n_ls=self.specs['n_ls'],
                               nonlin=tf.nn.relu,
                               filter_length=self.specs['filter_length'],
                               stride=self.specs['stride'],
                               pooling=self.specs['pooling'],
-                              padding=self.specs['padding'])
+                              padding=self.specs['padding'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
+
         self.lstm = LSTMv1(scope="lstm",
                            size=self.specs['rnn_units'],
                            dropout=self.specs['rnn_dropout'],
                            nonlin=self.specs['rnn_nonlin'],
-                           unit_forget_bias=self.specs['rnn_forget_bias'])
+                           unit_forget_bias=self.specs['rnn_forget_bias'],
+                           kernel_regularizer=self.l1_l2,
+                           bias_regularizer=self.l1_l2,
+                           return_sequences=self.specs['rnn_seq'])
 
-        self.fin_fc = Dense(size=self.out_dim, nonlin=tf.identity,
-                            dropout=self.rate)
+        # self.fin_fc = Dense(size=self.out_dim, nonlin=tf.identity,
+        #                     dropout=self.rate,
+        #                     kernel_regularizer=self.l1_l2,
+        #                     bias_regularizer=self.l1_l2)
+
+# self.l1 = self.add_loss(specs['l1']*tf.reduce_sum(tf.abs(self.trainable_variables)))
+# self.l2 = self.add_loss(specs['l2']*tf.nn.l2_loss(self.trainable_variables))
 
     def call(self, X):
-        x = self.demix(X)
-        x = self.tconv1(x)
-        x, _ = self.lstm(x)
-        x = self.fin_fc(x)
-        return x
+        orig_dim = len(X.shape)
+        # print(orig_dim)
+        x0 = X
+        if orig_dim > 4:
+            # Input shape: [batch_index, k, n_seq, n_ch, time]
+            x0 = tf.squeeze(X, axis=0)
+        elif orig_dim == 4:
+            k, n_seq, n_ch, time = X.shape    # [k, n_seq, n_ch, time]
+            x0 = X
+        elif orig_dim == 2:
+            n_ch, time = X.shape
+            x0 = tf.reshape(X, [1, 1, n_ch, time])  # [1, 1, n_ch, time]
+
+        # print('input x0', x0.shape)
+        x1 = self.demix(x0)                # [k, n_seq, time, 1, demix.size]
+        # print('demix x1', x1.shape)
+        x1a = tf.squeeze(x1, axis=0)       # [n_seq, time, 1, demix.size]
+        # print('squeezed x1a', x1a.shape)
+        x2 = self.tconv1(x1a)              # [n_seq, maxpool, tconv1.size]
+        # print('varconv x2', x2.shape)
+        col = tf.multiply(x2.shape[-1], x2.shape[-2])
+        x2a = tf.reshape(x2, [-1, col])  # [n_seq, maxpool*tconv1.size]
+        # print('reshaped x2a', x2a.shape)
+        x2b = tf.expand_dims(x2a, axis=0)  # [k, n_seq, maxpool*tconv1.size]
+        # print('expanded x2b', x2b.shape)
+        y_ = self.lstm(x2b)                # [k, n_seq, y_shape]
+        # print('lstm output y_', y_.shape)
+
+        if orig_dim > 4:
+            y_ = tf.expand_dims(y_, axis=0)  # [batch_index, k, n_seq, y_shape]
+            # print('expanded y_', y_.shape)
+        elif orig_dim == 2:
+            y_ = tf.reshape(y_, [-1])
+        # print('final output y_', y_.shape)
+        return y_
 
 
 class VARCNN(tf.keras.Model):
@@ -125,10 +169,7 @@ class VARCNN(tf.keras.Model):
         Parameters
         -----------
         specs : dict
-                dictionary of model-specific hyperparameters. Must include at
-                least model_path - path for saving a trained model. See
-                subclass definitions for details.
-
+                dictionary of model-specific hyperparameters.
         """
         super(VARCNN, self).__init__(name=name, **kwargs)
         self.scope = name
@@ -136,17 +177,26 @@ class VARCNN(tf.keras.Model):
         self.model_path = specs['model_path']
         self.rate = specs['dropout']
         self.out_dim = specs['out_dim']
+        self.l1_l2 = tf.keras.regularizers.l1_l2(l1=self.specs['l1'],
+                                                 l2=self.specs['l1'])
 
-        self.demix = DeMixing(n_ls=self.specs['n_ls'])
+        self.demix = DeMixing(n_ls=self.specs['n_ls'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2,
+                              axis=2)
         self.tconv1 = VARConv(scope="conv", n_ls=self.specs['n_ls'],
                               nonlin=tf.nn.relu,
                               filter_length=self.specs['filter_length'],
                               stride=self.specs['stride'],
                               pooling=self.specs['pooling'],
-                              padding=self.specs['padding'])
+                              padding=self.specs['padding'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
 
         self.fin_fc = Dense(size=self.out_dim, nonlin=tf.identity,
-                            dropout=self.rate)
+                            dropout=self.rate,
+                            kernel_regularizer=self.l1_l2,
+                            bias_regularizer=self.l1_l2)
 
     def call(self, X):
         x = self.demix(X)
@@ -155,104 +205,100 @@ class VARCNN(tf.keras.Model):
         return x
 
 
-def plot_history(history):
-    import matplotlib.pyplot as plt
+class VARDAE(tf.keras.Model):
+    # Migrated from dev.py NOT TESTED
+    """ VAR-CNN
 
-    plt.figure()
-    plt.subplot(211)
-    # Plot training & validation accuracy values
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('Model accuracy')
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Val'], loc='best')
-    plt.show()
+    For details see [1].
 
-    plt.subplot(212)
-    # Plot training & validation loss values
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Val'], loc='best')
-    plt.show()
+    Paramters:
+    ----------
+    var_params : dict
 
+    n_ls : int
+        number of latent components
+        Defaults to 32
 
-def _get_factors(n):
-    "Factorise integer value"
-    assert (n > 0) and not n % 1, "Cannot factor non-positive integer value"
-    return np.sort([
-        factor for i in range(1, int(n**0.5) + 1) if n % i == 0
-        for factor in (i, n//i)
-    ])
+    filter_length : int
+        length of spatio-temporal kernels in the temporal
+        convolution layer. Defaults to 7
 
+    stride : int
+        stride of the max pooling layer. Defaults to 1
 
-def val_test_results(model, train, val, test, r_batch, event_names):
-    # %
-    # train, val, test = dataset.train, dataset.val, test_dataset
-    from sklearn.metrics import classification_report
+    pooling : int
+        pooling factor of the max pooling layer. Defaults to 2
 
-    def _get_elem(data, batches):
-        y = []
-        ii = 0
-        for _, j in data:
-            if ii >= batches:
-                break
-            else:
-                y.extend(j)
-                ii += 1
+    References:
+    -----------
+        [1]  I. Zubarev, et al., Adaptive neural network classifier for
+        decoding MEG signals. Neuroimage. (2019) May 4;197:425-434
+    """
 
-        return np.asarray(y)
+    def __init__(self, specs, name='var-cnn-autoencoder', **kwargs):
+        """
+        Parameters
+        -----------
+        specs : dict
+                dictionary of model-specific hyperparameters.
+        """
+        super(VARDAE, self).__init__(name=name, **kwargs)
+        self.scope = name
+        self.l1_l2 = tf.keras.regularizers.l1_l2(l1=self.specs['l1'],
+                                                 l2=self.specs['l1'])
 
-    def _get_elem_g(dataset, batches):
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
-        y = []
-        ii = 0
-        with tf.compat.v1.Session() as sess:
-            while True:
-                if ii >= batches:
-                    break
-                else:
-                    _, j = sess.run(next_element)
-                    y.extend(j)
-                    ii += 1
+        self.demix = DeMixing(n_ls=self.specs['n_ls'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
 
-        return np.asarray(y)
+        self.tconv1 = VARConv(scope="var-conv1",
+                              n_ls=self.specs['n_ls'],
+                              nonlin_out=tf.nn.relu,
+                              filter_length=self.specs['filter_length'],
+                              stride=self.specs['stride'],
+                              pooling=self.specs['pooling'],
+                              padding=self.specs['padding'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
 
-    try:
-        y_train = _get_elem(train, r_batch)
-        y_val = _get_elem(val, 1)
-        y_test = _get_elem(test, 1)
-    except Exception:
-        y_train = _get_elem_g(train, r_batch)
-        y_val = _get_elem_g(val, 1)
-        y_test = _get_elem_g(test, 1)
+        self.tconv2 = VARConv(scope="var-conv2",
+                              n_ls=self.specs['n_ls']//2,
+                              nonlin_out=tf.nn.relu,
+                              filter_length=self.specs['filter_length']//2,
+                              stride=self.specs['stride'],
+                              pooling=self.specs['pooling'],
+                              padding=self.specs['padding'],
+                              kernel_regularizer=self.l1_l2,
+                              bias_regularizer=self.l1_l2)
 
-    # Compare performance between Validation and Test data
-    tmp = model.predict(train, steps=r_batch)
-    r_pred = np.argmax(tmp, axis=1)
+        self.encoding_fc = Dense(size=self.specs['out_dim'],
+                                 nonlin=tf.identity,
+                                 dropout=self.rate,
+                                 kernel_regularizer=self.l1_l2,
+                                 bias_regularizer=self.l1_l2)
 
-    tmp = model.predict(val, steps=1)
-    v_pred = np.argmax(tmp, axis=1)
+        self.deconv = DeConvLayer(n_ls=self.specs['n_ls'],
+                                  n_ch=self.dataset.h_params['n_ch'],
+                                  n_t=self.dataset.h_params['n_t'],
+                                  filter_length=self.specs['filter_length'],
+                                  flat_out=False,
+                                  kernel_regularizer=self.l1_l2,
+                                  bias_regularizer=self.l1_l2)
 
-    tmp = model.predict(test, steps=1)
-    t_pred = np.argmax(tmp, axis=1)
-
-    print('-------------------- TRAINING ----------------------\n',
-          classification_report(y_train, r_pred, target_names=event_names))
-    print('-------------------- VALIDATION ----------------------\n',
-          classification_report(y_val, v_pred, target_names=event_names))
-    print('----------------------- TEST -------------------------\n',
-          classification_report(y_test, t_pred, target_names=event_names))
+    def call(self, X):
+        x = self.demix(self.X)
+        x = self.tconv1(x)
+        x = self.tconv2(x)
+        encoder = self.encoding_fc(x)
+        decoder = self.deconv(encoder)
+        return decoder
 
 
 def test_model():
     # %%
     from mneflow import Dataset
     from presets import preset_data, get_subset, model_parameters
+    from keras_utils import plot_history, report_results
 
     preset = 'squid'
     dropbad = True
@@ -272,42 +318,38 @@ def test_model():
 
     print(r_samples, t_batch, v_batch)
 
-    # Factor the amount of training samples
-    tmp = _get_factors(r_samples)
+    n_batch = 250  # batch size
 
-    # find the closest divisor to eval_stop
-    eval_stop = 250  # after how many samples do we want to evaluate
-    n_batch = tmp[np.argmin(abs(tmp - eval_stop))]
+    # after how many batches to evaluate
+    steps = 1 if n_batch >= r_samples else r_samples // n_batch
 
-    # case where the amount of samples is smaller than the desired eval_stop
-    r_batch = 1 if n_batch == r_samples else r_samples // n_batch
-
-    # batch the dataset according to that value
+    # batch the train dataset
     dataset = Dataset(meta, train_batch=n_batch, class_subset=subset,
                       pick_channels=None, decim=None)
 
     # load test dataset without batching
-    test_dataset = dataset._build_dataset(meta['test_paths'], n_batch=None)
+    test_data = dataset._build_dataset(meta['test_paths'], n_batch=None)
 
-    # % Initialise model parameters
+    # Initialise model parameters
     graph_specs, _, _ = model_parameters(preset, meta['savepath'])
-    graph_specs['out_dim'] = dataset.h_params['n_classes']
+    graph_specs['out_dim'] = dataset.h_params['y_shape']
 
     opt = tf.keras.optimizers.Adam()
     loss = 'sparse_categorical_crossentropy'
     callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 
-    # % build model
+    # build model
     model = VARCNN(graph_specs)
     model.compile(loss=loss, optimizer=opt, metrics=['accuracy'])
     history = model.fit(dataset.train, validation_data=dataset.val,
-                        callbacks=[callback], epochs=2000,
-                        steps_per_epoch=r_batch, validation_steps=1)
-    results = model.evaluate(test_dataset, steps=1, verbose=1)
-    # %
+                        callbacks=[callback], epochs=2,
+                        steps_per_epoch=steps, validation_steps=v_batch)
+
+    results = model.evaluate(test_data, steps=t_batch, verbose=1)
+
     plot_history(history)
 
-    val_test_results(dataset.train, dataset.val, test_dataset, r_batch)
+    report_results(dataset.train, dataset.val, test_data, steps, subset_names)
 
 
 # %%
