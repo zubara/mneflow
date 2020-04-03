693cab85b5edb312d5389da13af7fbcc7805810a
Zubarev Ivan <zubarei1@vdiubuntu015.org.aalto.fi>
2020-04-03 17:00:37 +0300

    Halfway commit: working refactored layers and BaseModel

--- mneflow/keras_layers.py
+++ mneflow/keras_layers.py
@@ -15,73 +15,6 @@ from tensorflow.keras.activations import relu
 # TODO: add vgg_block layer
 
 
-class Dense(layers.Layer):
-    """
-    Fully-connected layer
-    """
-    def __init__(self, scope="fc", size=None, dropout=.5, nonlin=tf.identity,
-                 kernel_regularizer=None, bias_regularizer=None, **args):
-        assert size, "Must specify layer size (num nodes)"
-        super(Dense, self).__init__(name=scope, **args)
-        self.scope = scope
-        self.size = size
-        self.dropout = dropout
-        self.nonlin = nonlin
-        self.kernel_regularizer = kernel_regularizer
-        self.bias_regularizer = bias_regularizer
-        print(self.scope, 'init : OK')
-
-    def get_config(self):
-        config = super(Dense, self).get_config()
-        config.update({'scope': self.scope, 'size': self.size,
-                       'dropout': self.dropout, 'nonlin': self.nonlin})
-        return config
-
-    def build(self, input_shape):
-        super(Dense, self).build(input_shape)
-        # print(input_shape)
-        if len(input_shape) > 2:
-            self.flatsize = np.prod(input_shape[1:]).value
-        else:
-            self.flatsize = input_shape[1].value
-
-        print(self.scope, ':::', self.flatsize, self.size)
-        self.w = self.add_weight(shape=[self.flatsize, self.size],
-                                 initializer='he_uniform',
-                                 regularizer=self.kernel_regularizer,
-                                 trainable=True,
-                                 name='fc_weights',
-                                 dtype=tf.float32)
-
-        self.b = self.add_weight(shape=[self.size],
-                                 initializer=Constant(0.1),
-                                 regularizer=self.bias_regularizer,
-                                 trainable=True,
-                                 name='fc_bias',
-                                 dtype=tf.float32)
-
-        print(self.scope, 'build : OK')
-
-    @tf.function
-    def call(self, x, training=None):
-        """Dense layer currying, to apply layer to any input tensor `x`"""
-        with tf.name_scope(self.scope):
-            if len(x.shape) > 2:  # flatten if input is not 2d array
-                # print(self.flatsize)
-                x = tf.reshape(x, [-1, self.flatsize])
-
-            tmprate = 0.5 if training else 0.0
-            tmpw = tf.nn.dropout(self.w, rate=tmprate)
-
-            tmp = tf.matmul(x, tmpw)
-            # print('matmul shape:', tmp.shape)
-            tmp = tmp + self.b
-            # print('added bias shape:', tmp.shape)
-            tmp = self.nonlin(tmp, name='out')
-            # print('after nonlin:', tmp.shape)
-            return tmp
-
-
 class LFTConv(layers.Layer):
     """
     Stackable temporal convolutional layer, interpreatble (LF)
@@ -237,57 +170,6 @@ class VARConv(layers.Layer):
             return conv
 
 
-class DeMixing(layers.Layer):
-    """
-    Spatial demixing Layer
-    """
-    def __init__(self, scope="de-mix", n_ls=32,  axis=2, nonlin=tf.identity,
-                 kernel_regularizer=None, bias_regularizer=None, **args):
-        super(DeMixing, self).__init__(name=scope, **args)
-        self.scope = scope
-        self.size = n_ls
-        self.nonlin = nonlin
-        self.axis = axis
-        self.kernel_regularizer = kernel_regularizer
-        self.bias_regularizer = bias_regularizer
-
-        print(self.scope, 'init : OK')
-
-    def get_config(self):
-        config = super(DeMixing, self).get_config()
-        config.update({'scope': self.scope, 'size': self.size,
-                       'nonlin': self.nonlin, 'axis': self.axis})
-        return config
-
-    def build(self, input_shape):
-
-        super(DeMixing, self).build(input_shape)
-        self.W = self.add_weight(
-                shape=(input_shape[self.axis].value, self.size),
-                initializer='he_uniform',
-                regularizer=self.kernel_regularizer,
-                trainable=True,
-                name='dmx_weights',
-                dtype=tf.float32)
-
-        self.b_in = self.add_weight(shape=([self.size]),
-                                    initializer=Constant(0.1),
-                                    regularizer=self.bias_regularizer,
-                                    trainable=True,
-                                    name='bias',
-                                    dtype=tf.float32)
-        print(self.scope, 'built : OK')
-
-    @tf.function
-    def call(self, x, training=None):
-        with tf.name_scope(self.scope):
-            demix = tf.tensordot(x, self.W, axes=[[self.axis], [0]],
-                                 name='de-mix')
-            demix = self.nonlin(demix + self.b_in)
-            # x_reduced = tf.expand_dims(demix, -2)
-
-            print('dmx', demix.shape)
-            return demix
 
 
 class ConvDSV(layers.Layer):
