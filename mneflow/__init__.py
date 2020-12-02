a9ffeeeba9bf297f9c16fcfbdedc26a34db1187d
Ivan Zubarev <ivan.zubarev@aalto.fi>
2020-12-02 16:39:32 +0200

    updated init.py

--- mneflow/__init__.py
+++ mneflow/__init__.py
@@ -1,7 +1,5 @@
 from . import models
 from . import layers
 from .utils import produce_tfrecords, load_meta
-# from .utils import logger, plot_cm
 from .data import Dataset
-from .optimize import Optimizer
 
