fee67a99a6b8fad4f55084a9e14188e54388901f
Ivan Zubarev <ivan.zubarev@aalto.fi>
2026-09-21 16:26:19 +0300

    Setting version from _pkg_version("mneflow")

--- mneflow/__init__.py
+++ mneflow/__init__.py
@@ -28,4 +28,9 @@ from .data import Dataset
 from .models import VARCNN, Deep4, FBCSP_ShallowNet, EEGNet
 from .lfcnn import LFCNN
 from .meta import MetaData
-mneflow.__version__ = '0.5.13dev'
+
+try:
+    from importlib.metadata import version as _pkg_version, PackageNotFoundError
+    mneflow.__version__ = _pkg_version("mneflow")
+except PackageNotFoundError:
+    mneflow.__version__ = "0+unknown"
